58cae49722960fdbb3993b293fcf1a34de6a9814
Luis Xutuc <158003827+xxxlxlx@users.noreply.github.com>
2024-02-01 23:04:35 -0600

    FKii.sh
    efqui

---
+++ FKii.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+echo " _______  _       __________________"
+echo "(  ____ \| \    /\\__   __/\__   __/"
+echo "| (    \/|  \  / /   ) (      ) ("
+echo "| (__    |  (_/ /    | |      | |"
+echo "|  __)   |   _ (     | |      | |"
+echo "| (      |  ( \ \    | |      | |"
+echo "| )      |  /  \ \___) (______) (___"
+echo "|/       |_/    \/\_______/\_______/"
+echo "Automatic Flatpak installer on Linux"
+echo "-------------------------------------" 
+echo "Script by Lu1s X"
+echo "-------------------------------------"
+cat << OSS
+Select your operating system
+OSS
+
+cat << DISTROS
+
+(1) Ubuntu
+
+(2) ArchLinux
+
+(3) OpenSuse
+
+(4) SlackWare / Beta*
+DISTROS
